12096fd55fdc6275dcb88b45376397f263229ee7
tothricsaj <tothricsaj@gmail.com>
2023-08-23 12:13:47 +0200

    feat: part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Blog`,
+    title: `My Blog and my first Gatsby site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
