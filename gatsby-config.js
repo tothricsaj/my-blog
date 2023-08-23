cd67a5597fd8e0545fc404ddbf0831c11e8c56c9
tothricsaj <tothricsaj@gmail.com>
2023-08-23 11:47:56 +0200

    feat: part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `My Blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
