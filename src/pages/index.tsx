b27545d5ccce930a042059363f25dd301a2a7414
tothricsaj <tothricsaj@gmail.com>
2025-01-09 13:05:27 +0100

    feat: adde static image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Layout from "../components/layout";
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome my blog!">
       <p>This is my pure Gatsby blog....yheee!!!</p>
+      <StaticImage
+        alt="Clifford, the pitbull"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   );
 };
